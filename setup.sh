25990b8d882fec6d9e059459bfbb0b03088c9032
seekerkrt <seekerkrt@gmail.com>
2019-01-23 20:56:51 +0900

    Added vscode/settings.json and Updated

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -u
 
 THIS_DIR=$(cd $(dirname $0); pwd)
-
+# ".??* => ２文字以上の.がついたファイルやフォルダを、ホームディレクトリ以下にリンクを置く"
 cd $THIS_DIR
 echo "Start setup..."
 for dotfile in .??*; do
@@ -10,7 +10,9 @@ for dotfile in .??*; do
     [ "$dotfile" = ".git" ] && continue
     [ "$dotfile" = ".gitconfig.local.template" ] && continue
     [ "$dotfile" = ".gitmodules" ] && continue
-# リンクを貼る  
-    ln -sfnv ~/dotfiles/"$dotfile" ~/
+# リンクを貼る
+    ln -sfnv $THIS_DIR/"$dotfile" ~/
 done
 
+# VSCodeの設定ファイル類をリンク置く
+ln -fnsv $THIS_DIR/vscode/settings.json ~/.config/Code/User/settings.json
