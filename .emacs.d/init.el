f817ca8ff3977ecf3fee0f07037c929ce91f8273
seekerkrt <4817613+seekerkrt@users.noreply.github.com>
2022-10-11 22:17:13 +0900

    Updated emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -106,20 +106,3 @@
 (autoload 'python-mode "python-mode.el" "Python mode." t)
 (setq auto-mode-alist (append '(("/.*\.py\'" . python-mode)) auto-mode-alist))
 
-
-;; flycheck
-;(require 'flycheck)
-;;(add-hook 'after-init-hook 'global-flycheck-mode)
-;;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(rust-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
