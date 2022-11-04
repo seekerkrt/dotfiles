35b3f60eaaf518dffbb05c529a5419d02ad2ee1e
seekerkrt <seekerkrt@gmail.com>
2022-11-05 02:26:35 +0900

    WezTerm 設定を更新

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,14 +1,19 @@
 local wezterm = require 'wezterm'
 return {
-   color_scheme = "MonokaiDark (Gogh)",
-
+--   color_scheme = "MonokaiDark (Gogh)",
+--   color_scheme = "OneHalfDark",
+--   color_scheme = "nord",
+--   color_scheme = "Solarized Dark Higher Contrast",
+   color_scheme = "Monokai Vivid",
+   
    font = wezterm.font "Mgen+ 1mn",
-   font_size = 12.0,
+   font_size = 11.0,
 
-   initial_cols = 140,
+   initial_cols = 120,
    initial_rows = 35,
+   enable_scroll_bar = true, 
    scrollback_lines = infinite,
    enable_scroll_bar = true,
    use_ime = true,
-   window_background_opacity = 0.85,
+   window_background_opacity = 0.80,
 }
