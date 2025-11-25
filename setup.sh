349027330f3af8df6625b129db7f9ecac1e7c9a8
seekerkrt <4817613+seekerkrt@users.noreply.github.com>
2025-11-25 10:43:38 +0900

    stowに対応、構造変更

--- setup.sh
+++ setup.sh
@@ -1,24 +1,7 @@
 #! /usr/bin/env sh
 set -u
 
-THIS_DIR=$(cd $(dirname $0); pwd)
-# ".??* => ２文字以上の.がついたファイルやフォルダを、ホームディレクトリ以下にリンクを置く"
-cd $THIS_DIR
-echo "Start setup..."
-for dotfile in .??*; do
-    # 指定のファイルやディレクトリを除外
-    [ "$dotfile" = ".git" ] && continue
-    [ "$dotfile" = ".gitconfig.local.template" ] && continue
-    [ "$dotfile" = ".gitmodules" ] && continue
-    [ "$dotfile" = ".config" ] && continue
-    # リンクを貼る
-    ln -sfnv $THIS_DIR/"$dotfile" ~/
-done
-
+stow ssh zsh vim git scripts clang-format alacritty tmux xorg wezterm vscode conky
 # sshディレクトリのパーミッションを適切にする
 chmod -v 700 ~/.ssh
 chmod -v 600 ~/.ssh/*
-
-# VSCodeの設定ファイル類をリンク置く
-mkdir -pv ~/.config/Code/User/
-ln -sfnv $THIS_DIR/vscode/settings.json ~/.config/Code/User/settings.json
