6ebbdc6d47665ea161d450f586fb249c67131eaa
seekerkrt <seekerkrt@gmail.com>
2025-11-25 10:47:47 +0900

    stowに対応、構造変更

--- setup.sh
+++ setup.sh
@@ -1,7 +1,18 @@
 #! /usr/bin/env sh
 set -u
 
-stow ssh zsh vim git scripts clang-format alacritty tmux xorg wezterm vscode conky
+stow ssh
+stow zsh
+stow vim
+stow git
+stow scripts
+stow clang-format
+stow alacritty
+stow tmux
+stow xorg
+stow wezterm
+stow vscode
+stow conky
 # sshディレクトリのパーミッションを適切にする
 chmod -v 700 ~/.ssh
 chmod -v 600 ~/.ssh/*
