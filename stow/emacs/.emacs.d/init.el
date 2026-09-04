2d0a82b80a0766d8530173cfb8ca56a633d8da6e
seekerkrt <4817613+seekerkrt@users.noreply.github.com>
2026-09-05 04:39:40 +0900

    stow: emacs更新

--- stow/emacs/.emacs.d/init.el
+++ stow/emacs/.emacs.d/init.el
@@ -1,108 +1,145 @@
-;; シンボリックリンクを強制的にたどる＝＞プロンプトを出さない
+;;; init.el --- Emacs configuration -*- lexical-binding: t; -*-
+
+;; ---------------------------------------------------------------------------
+;; Symlink
+;; ---------------------------------------------------------------------------
+
+;; シンボリックリンク先を編集するときの確認を省略
 (setq vc-follow-symlinks t)
+
+;; 常に実体のパスとしてファイルを扱う
 (setq-default find-file-visit-truename t)
-;;
+
+;; この init.el が置かれているディレクトリを Emacs 設定ディレクトリとする
 (when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
+  (setq user-emacs-directory
+        (file-name-directory load-file-name)))
 
-;;言語環境指定
-(set-language-environment "japanese")
-(set-default-coding-systems 'utf-8)
-(set-terminal-coding-system 'utf-8)
+
+;; ---------------------------------------------------------------------------
+;; Language / Encoding
+;; ---------------------------------------------------------------------------
+
+(set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
-(setq coding-system-for-read 'utf-8)
-(setq coding-system-for-write 'utf-8)
-;;行番号表示指定
-;(require 'linum)
-;(global-linum-mode t)
-;(setq linum-format "%4d ")
-(if (version<= "26.0.50" emacs-version)
-      (global-display-line-numbers-mode))
 
 
-;;フォント指定
+;; ---------------------------------------------------------------------------
+;; Display
+;; ---------------------------------------------------------------------------
+
+;; 行番号
+(global-display-line-numbers-mode 1)
+
+;; モードラインに現在位置を表示
+(line-number-mode 1)
+(column-number-mode 1)
+
+;; ファイルサイズを表示
+(size-indication-mode 1)
+
+;; 現在の関数名を表示
+(which-function-mode 1)
+
+;; 対応する括弧をハイライト
+(show-paren-mode 1)
+(setq show-paren-style 'mixed)
+
+;; 選択範囲をハイライト
+(transient-mark-mode 1)
+(set-face-background 'region "DeepSkyBlue")
+
+
+;; ---------------------------------------------------------------------------
+;; Font
+;; ---------------------------------------------------------------------------
+
 (set-face-attribute 'default nil
                     :family "MyricaM M"
-                    :height 12)
-(set-frame-font "MyricaM M-9")
+                    :height 120)
 
-;; メニューバーを消す
-(menu-bar-mode 0)
-;; スタートアップメッセージを表示しない
-(setq inhibit-startup-message t)
-; yes/no => y/n
-(defalias 'yes-or-no-p 'y-or-n-p)
-;;Auto-Save
-(setq make-backup-files nil)
-(setq auto-save-default nil)
 
-;; 対応する括弧をハイライトする
-(show-paren-mode 1)
-;;画面内に対応する括弧がある場合は括弧だけを、ない場合は括弧で囲まれた部分をハイライト
-(setq show-paren-style 'mixed)
-;; カーソル位置の桁数をモードライン行に表示する
-(column-number-mode 1)
-;; カーソル位置の行数をモードライン行に表示する
-(line-number-mode 1)
-;;ファイルサイズを表示
-(size-indication-mode t)
+;; ---------------------------------------------------------------------------
+;; Frame
+;; ---------------------------------------------------------------------------
 
-;; モードラインや見た目の設定
-(line-number-mode t)                ;; 行数
-(column-number-mode t)              ;; 桁数
-(which-function-mode t)
 (setq default-frame-alist
-      (append (list
-       '(set-foreground-color . "AntiqueWhite")  ; 前景色
-       '(set-background-color . "black") ; 背景色
-;      '(cursor-color     . "DarkGreen")  ; カーソル色
-       '(set-cursor-color     . "Gray")  ; カーソル色
-       '(set-frame-parameter nil 'alpha 80);透過度
-       )
+      (append
+       '((foreground-color . "AntiqueWhite")
+         (background-color . "black")
+         (cursor-color . "Gray")
+         (alpha-background . 80))
        default-frame-alist))
-;;適用?
+
+;; 最初のフレームにも同じ設定を適用
 (setq initial-frame-alist default-frame-alist)
 
-;;
-;; 選択範囲に色をつける
-(transient-mark-mode t)
-(set-face-background 'region "DeepSkyBlue") ;選択範囲の色
+
+;; ---------------------------------------------------------------------------
+;; UI
+;; ---------------------------------------------------------------------------
+
+;; メニューバーを非表示
+(menu-bar-mode -1)
+
+;; スタートアップ画面を表示しない
+(setq inhibit-startup-message t)
+
+;; yes / no ではなく y / n
+(setq use-short-answers t)
 
 
+;; ---------------------------------------------------------------------------
+;; Backup / Auto Save
+;; ---------------------------------------------------------------------------
+
+;; バックアップファイルを作成しない
+(setq make-backup-files nil)
+
+;; Auto Saveを無効化
+(setq auto-save-default nil)
+
+
+;; ---------------------------------------------------------------------------
 ;; Indent
+;; ---------------------------------------------------------------------------
+
 (setq-default indent-tabs-mode nil)
-(setq-default c-basic-offset 4)
 (setq-default tab-width 4)
+(setq-default c-basic-offset 4)
 
 
+;; ---------------------------------------------------------------------------
+;; Major Modes
+;; ---------------------------------------------------------------------------
 
-;;カラーテーマ設定
-;(require 'color-theme)
-;(color-theme-initialize)
-;(load-theme 'manoj-dark t)
-;(load-theme 'tango-dark t)
-;(load-theme 'deep-blue t)
+;; Lua
+(autoload 'lua-mode "lua-mode"
+  "Major mode for editing Lua source files." t)
+(add-to-list 'auto-mode-alist '("\\.lua\\'" . lua-mode))
+
+;; Ruby
+(add-to-list 'auto-mode-alist '("\\.rb\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
+
+;; Python
+(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
+
+;; Rust
+(autoload 'rust-mode "rust-mode"
+  "Major mode for editing Rust source files." t)
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
 
 
-;;Lua-mode
-(setq auto-mode-alist (cons '("\.lua$" . lua-mode) auto-mode-alist))
-(autoload 'lua-mode "lua-mode" "Lua mode." t)
+;; ---------------------------------------------------------------------------
+;; Color Theme
+;; ---------------------------------------------------------------------------
 
-;;Ruby-mode
-(autoload 'ruby-mode "ruby-mode"
-  "Mode for editing ruby source files" t)
-(add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
-(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+;; 必要なら有効化
+;; (load-theme 'manoj-dark t)
+;; (load-theme 'tango-dark t)
+;; (load-theme 'deep-blue t)
 
-;;Python-mode
-(autoload 'python-mode "python-mode.el" "Python mode." t)
-(setq auto-mode-alist (append '(("/.*\.py\'" . python-mode)) auto-mode-alist))
 
-;;Haskell-mode
-;(require 'haskell-mode-autoloads)
-;; auto-complete
-;(require 'auto-complete)
-;(require 'auto-complete-config)
-;(global-auto-complete-mode t)
-
+;;; init.el ends here
